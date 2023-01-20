68977681cf780e75d43f9b6474c197738b5120e6
Hephzibahbakre45 <ayomidebakre37@gmail.com>
2023-01-20 17:32:09 +0100

    add some lines

--- main1.c
+++ main1.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
+#include<stdbool.h>
 
 int main(){ 
 
     printf("Hello world, i am learning programming\n");
+    int time =20;
+    if (time < 18){
+        printf("good morning.");
+    } else {
+        printf("good evening.");
+    }
+
 
     return (0);
 
